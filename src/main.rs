018e4dd554aacce5f1089e5912a5736eb78b15c8
augi <heavymetalthings@gmail.com>
2023-03-31 19:56:33 +0200

    UwU

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+
+
+fn main() {
+    println!("Tu es Pierre et sur cette pierre, je bâtiai mon église");
+
+    let mut a: [i8;10] = [42;10];
+    //let mut b = 
+    a[5]=5;
+    println!("a: {:?}",a);
+    println!("second number {}", a[1])
+}
