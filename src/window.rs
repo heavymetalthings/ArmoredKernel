6e272589443f1bf26a06d3dcad8048ca31c456a2
augi <heavymetalthings@gmail.com>
2023-04-01 16:38:30 +0200

    16

---
+++ src/window.rs
@@ -0,0 +1,47 @@
+use iced::{button, Button, Column, Element, Sandbox, Settings, Text};
+
+pub struct MyApp {
+    button_state: button::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    ButtonClicked,
+}
+
+impl Sandbox for MyApp {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            button_state: button::State::new(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("My Iced App")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ButtonClicked => {
+                println!("Hello, world!");
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .push(Text::new("Click the button!"))
+            .push(Button::new(&mut self.button_state, Text::new("Click me!")).on_press(Message::ButtonClicked))
+            .into()
+    }
+}
+
+fn main() {
+    let settings = Settings::default();
+    match MyApp::run(settings) {
+        Ok(_) => {}
+        Err(e) => eprintln!("Application error: {:?}", e),
+    }
+}
